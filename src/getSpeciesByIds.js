82e3599d12967b5fd04435320246384c4c9dbf68
lcoelho3412 <leandrocoelho3@yahoo.com>
2022-05-22 20:43:45 -0300

    Adciona requisito getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  return species.filter((xablau) => ids.includes(xablau.id));
 }
 
+/* console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce','0938aa23-f153-4937-9f88-4858b24d6bce',
+'e8481c1d-42ea-4610-8e11-1752cfc05a46',)); */
 module.exports = getSpeciesByIds;
