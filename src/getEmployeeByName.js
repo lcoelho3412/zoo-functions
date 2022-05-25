a160f24546079ae916752668684fa2ebe16719ff
lcoelho3412 <leandrocoelho3@yahoo.com>
2022-05-24 23:31:46 -0300

    Adciona requisito getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  return employees
+    .find((item) => item.firstName === employeeName || item.lastName === employeeName) || {};
 }
 
 module.exports = getEmployeeByName;
