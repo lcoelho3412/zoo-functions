0b06263c0fe0962228578cc2ec34790a0e15e4a7
lcoelho3412 <leandrocoelho3@yahoo.com>
2022-05-25 19:03:16 -0300

    Adciona requisito countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return species.reduce((acc, { name, residents }) => {
+      acc[name] = residents.length;
+      return acc;
+    }, {});
+  }
+  if ('specie' in animal) {
+    const temAnimal = species.find((element) => element.name === animal.specie).residents;
+    if ('sex' in animal) {
+      return temAnimal
+        .filter((residentEvil) => residentEvil.sex === animal.sex).length;
+    }
+    return temAnimal.length;
+  }
 }
 
 module.exports = countAnimals;
