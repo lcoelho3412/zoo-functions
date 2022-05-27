927b42898d841a49b2989c28bde5f494f40e15d0
lcoelho3412 <leandrocoelho3@yahoo.com>
2022-05-27 16:58:48 -0300

    Adciona requisito getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animalFinder = species.find((element) => element.name === animal).residents;
+  return animalFinder.every((cage) => cage.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
