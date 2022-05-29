3f46bf5b2057782fcf896529d85265260773d186
lcoelho3412 <leandrocoelho3@yahoo.com>
2022-05-29 19:54:57 -0300

    Adciona test de getOpeningHours com corbetura de 90%

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,35 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Teste não passando argumentos. Deverá retornar um objeto com todos os horários', () => {
+    const expected = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+    const actual = getOpeningHours();
+    expect(expected).toEqual(actual);
+  });
+
+  it('Retorna "The zoo is closed" quando parâmetro é "Monday e 09:00-AM"', () => {
+    const actual = getOpeningHours('Monday', '09:00-AM');
+    const expected = 'The zoo is closed';
+    expect(actual).toEqual(expected);
+  });
+
+  it('Retorna "The zoo is open" quando parâmetro é "Tuesday e 09:00-AM" ', () => {
+    const actual = getOpeningHours('Tuesday', '09:00-AM');
+    const expected = 'The zoo is open';
+    expect(actual).toEqual(expected);
+  });
+
+  it('Retorna "The zoo is closed" quando parâmetro é "Wednesday e 09:00-PM"', () => {
+    const actual = getOpeningHours('Wednesday', '09:00-PM');
+    const expected = 'The zoo is closed';
+    expect(actual).toEqual(expected);
+  });
+});
