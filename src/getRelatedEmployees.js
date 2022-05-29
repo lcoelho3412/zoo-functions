7bdaa0a9dbc3e4a9a128f86fe27a2aff8c836390
lcoelho3412 <leandrocoelho3@yahoo.com>
2022-05-29 14:36:33 -0300

    Adciona requisito getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
   // seu código aqui
+  return employees.some((employee) =>
+    employee.managers.some((manager) => manager === id));
 }
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
+function getRelatedEmployees(id) {
+  if (!isManager(id)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return data.employees
+    .filter((employee) => employee.managers.some((manager) => manager === id))
+    .map((employee) => `${employee.firstName} ${employee.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
