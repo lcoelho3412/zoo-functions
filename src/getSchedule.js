7465a8bdc9fc2f8fe668b0d246ecfa3fe28f3b56
lcoelho3412 <leandrocoelho3@yahoo.com>
2022-05-31 15:47:25 -0300

    Ajuste linter

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,10 +1,6 @@
 const data = require('../data/zoo_data');
 
-const { species } = data;
-const { hours } = data;
-
 function getSchedule(scheduleTarget) {
-  return species.find((availability) => availability.name === scheduleTarget).availability;
 }
 console.log(getSchedule('lions'));
 module.exports = getSchedule;
