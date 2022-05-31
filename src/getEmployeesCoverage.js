2967ce89ae7d024d4dc27d12095080705ee2e9eb
lcoelho3412 <leandrocoelho3@yahoo.com>
2022-05-31 20:44:37 -0300

    Adiciona requisito getEmployeeCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,34 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+
+const nameTag = employees.map((info) => (
+  {
+    id: info.id,
+    fullName: `${info.firstName} ${info.lastName}`,
+    species: info.responsibleFor.map((id) => (
+      species.find((empId) => (empId.id === id)).name
+    )),
+    locations: info.responsibleFor.map((id) => (
+      species.find((specieLocation) => (specieLocation.id === id)).location
+    )),
+  }
+));
+
+function getEmployeesCoverage(obj) {
+  const employee = obj && nameTag.find((info) => (
+    obj.id ? info.id === obj.id : info.fullName.includes(obj.name)
+  ));
+
+  if (employee) {
+    return employee;
+  }
+
+  if (obj) {
+    throw new Error('Informações inválidas');
+  }
+
+  return nameTag;
 }
 
 module.exports = getEmployeesCoverage;
