75725fd48489ee362cffa33834cc0d46932378fd
lcoelho3412 <leandrocoelho3@yahoo.com>
2022-05-31 15:41:41 -0300

    Adciona requisito getOldestFromSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const handlerFinder = employees
+    .find((resposibility) => resposibility.id === id)
+    .responsibleFor[0];
+  const goGetAnimal = species.find((animal) => animal.id === handlerFinder).residents
+    .sort((a, b) => b.age - a.age)[0];
+  return [goGetAnimal.name, goGetAnimal.sex, goGetAnimal.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+const { species } = data;
+const { hours } = data;
 
+function getSchedule(scheduleTarget) {
+  return species.find((availability) => availability.name === scheduleTarget).availability;
+}
+console.log(getSchedule('lions'));
 module.exports = getSchedule;
